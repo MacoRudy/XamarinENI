e7915ac1a66a050cc395bdb579633a8d6dbb2342
Rudy MACORIGH <rudy.macorigh2020@campus-eni.fr>
2020-09-30 10:15:37 +0200

    TP module 06

--- TP1/TP1/MainPage.xaml.cs
+++ TP1/TP1/MainPage.xaml.cs
@@ -8,14 +8,49 @@ using Xamarin.Forms;
 
 namespace TP1
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
+ 
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            this.connection.Clicked += Connection_Clicked;
+        }
+
+        private void Connection_Clicked(object sender, EventArgs e)
+        {
+            string password = this.password.Text.ToString();
+            string id = this.id.Text.ToString();
+            string memo = this.memo.IsToggled.ToString();
+            System.Console.WriteLine(id + " " + password + " " + memo);
+            bool okId = false;
+            bool okPwd = false;
+            if (id.Trim().Length < 3 )
+            {
+                this.error.Text = "L'identifiant doit avoir au moins 3 caractères";
+                this.error.IsVisible = true;
+
+            } else
+            {
+                okId = true;
+            }
+
+            if (password.Trim().Length < 6)
+            {
+                this.error.Text = "Le mot de passe doit avoir au moins 6 caractères";
+                this.error.IsVisible = true;
+            } else
+            {
+                okPwd = true;
+            }
+
+            if (okId && okPwd)
+            {
+                this.scroll.IsVisible = true;
+                this.login.IsVisible = false;
+                this.error.IsVisible = false;
+            }
         }
     }
 }
